8f7c265e2a5fdbc2a29efb7a64992427da4b2b63
vishal <vishalmahesh.study@gmail.com>
2024-05-11 11:11:28 +0530

    ui: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import SectionContainer from "./section-container";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+
+const Header = () => {
+  const user = undefined;
+  const isAdmin = false;
+  return (
+    <header className="sticky inset-x-0 top-0 z-[100] h-14 border-b border-zinc-200 bg-white/75 backdrop-blur-lg transition-all">
+      <SectionContainer>
+        <div className="flex h-full items-center justify-between border-b border-zinc-200">
+          <Link href="/">Cazeey</Link>
+
+          <div className="spaxe-x-4 flex h-full items-center">
+            {user ? (
+              <>
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign out
+                </Link>
+                {isAdmin && (
+                  <Link
+                    href="/api/auth/logout"
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
+                  >
+                    Dashboard ✨
+                  </Link>
+                )}
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "default",
+                    className: "hidden items-center gap-1 sm:flex",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign up
+                </Link>
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                    className: "hidden items-center gap-1 sm:flex",
+                  })}
+                >
+                  Login
+                </Link>
+                <div className="mx-4 hidden h-8 w-px bg-zinc-200 sm:block" />
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "default",
+                    className: "hidden items-center gap-1 sm:flex",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </SectionContainer>
+    </header>
+  );
+};
+
+export default Header;
